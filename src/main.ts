6cda8a4639bcbe8fcd2ca11e5910205045f02534
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2025-01-20 15:29:05 +0100

    init commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import "./style.css";
+
+const form = document.getElementById("form") as HTMLFormElement;
+const file = document.getElementById("file") as HTMLInputElement;
+
+form.addEventListener("submit", async (ev) => {
+	ev.preventDefault();
+
+	const text = await file.files?.item(0)?.text();
+
+	if (!text) {
+		throw new Error("Failed to read file");
+	}
+
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(text, "text/xml");
+
+	const diffuse = doc.querySelector("diffuse");
+	diffuse!.textContent = "";
+
+	const armature = doc.querySelector("node#Armature");
+	const scene = doc.querySelector("visual_scene#Scene");
+
+	const children: Node[] = [];
+	armature?.childNodes.forEach((v) => {
+		if (v.nodeName == "NODE") children.push(v.cloneNode(true));
+	});
+
+	armature?.remove();
+
+	scene?.append(...children);
+
+	const serializer = new XMLSerializer();
+	const content = serializer.serializeToString(doc);
+
+	download(file.files?.item(0)?.name!, content);
+});
+
+function download(filename: string, text: string) {
+	const element = document.createElement("a");
+	element.setAttribute(
+		"href",
+		"data:text/plain;charset=utf-8," + encodeURIComponent(text),
+	);
+	element.setAttribute("download", filename);
+
+	element.style.display = "none";
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
